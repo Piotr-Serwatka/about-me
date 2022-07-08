0ee60eb90f1bc1b55a4a740898daf29a2427991f
Piotr Serwatka <[piotr.serwatka@grupawp.pl]>
2022-07-08 21:40:57 +0200

    update exp

--- src/components/about/Skills.vue
+++ src/components/about/Skills.vue
@@ -38,7 +38,7 @@
 
       <div class="col-12 col-md-6">
         <h2 class="h1">9 months of experience</h2>
-        <p>Im working as junior frontend developer for 9 months. Mainly im working with creating a new features and
+        <p>Im working as junior frontend developer for 1,5 year. Mainly im working with creating a new features and
           solving errors in maintenance departament based on Vue.js and JavaScript technology. </p>
       </div>
     </div>
